b03f355a9d11c445820c05bfeb2f25973c896998
Ihor Tymoshenko <goracio84@gmail.com>
2021-05-21 19:50:24 +0300

    remove keys

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIAXU4PD254C3MGBFQB"
-  secret_key = "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"
+  access_key = "AWS-key"
+  secret_key = "AWS-key"
 }
